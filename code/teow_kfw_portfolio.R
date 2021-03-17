190ec1c77023b01a47420e3daacff88e160a8071
Johannes Schielein <johannes.schielein@posteo.net>
2021-03-17 16:58:29 +0100

    create script

---
+++ code/teow_kfw_portfolio.R
@@ -0,0 +1,14 @@
+#  teow_kfw_portfolio
+# Authors: Johannes Schielein
+# Purpose: This script uses the routines from the TEOW rmd files to process the whole KfW portfolio
+# Notes: 
+  # The teow rawdata is stored in the mapme datalake and is not part of this repository.
+  # More information on where to download the TEOW data and how to process it is given in the project documentation website
+
+
+# load required libraries
+library("sf")
+library("dplyr")
+
+# load data
+read_sf("")
