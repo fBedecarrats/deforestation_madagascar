318d580244c39cb7ee7aef26c09e43221b427df5
Yota <yota@dsvm-geospatial.e1jxpv4mbzsenbsgf00fvcpi2f.ax.internal.cloudapp.net>
2021-05-20 08:35:25 +0000

    Create script

---
+++ code/projectdata.R
@@ -0,0 +1,14 @@
+# script for analyzing  project information descriptively
+
+library("tidyverse")
+library("sf")
+
+# working directory - setwd()
+projectdata_db<-
+  read_csv("../../datalake/mapme.protectedareas/input/project-data/xyz.csv")
+
+# ----- data wrangling ----
+
+# ---- analyze xyz ---- 
+
+# ---- merge with KfW data ---- 
