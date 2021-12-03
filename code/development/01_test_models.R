c865c1a098cca60628760b9d056ad797420ad03d
melvinhlwong <m.wong@gmx.de>
2021-12-03 12:16:35 +0000

    first model pre-matching. FE loss on forest cover loss.
    adj. Rsqr is negative

--- code/development/01_test_models.R
+++ code/development/01_test_models.R
@@ -21,45 +21,56 @@ setwd("~/shared/datalake/mapme.protectedareas")
 
 
 ### define panel
-panel.df <- pdata.frame(Conflict_data, index=c("UID","year"))
+out2015.df <- read.csv("./output/tabular/regression_input/out2015.csv")
+panel.df <- pdata.frame(out2015.df, index=c("uid_myear","year_standard"))
 
 ### run models
-m1 <- plm(loss~treatment, data=panel.df, model=("within")) 
+m1 <- plm(loss ~ treatment_disb, data=panel.df, model=("within")) 
 summary(m1)
+
+m2<- plm(loss ~ treatment_disb + year, data=panel.df, model=("within")) 
+summary(m2)
+
+m3 <- plm(loss ~ disbursement_proj, data=panel.df, model=("within")) 
+summary(m3)
+
+m4<- plm(loss ~ disbursement_proj + year, data=panel.df, model=("within")) 
+summary(m4)
 
 
 ### check table
-stargazer(m1,m2,m3 ,title="Results",type="text", align=TRUE)
+stargazer(m1, m2, m3, m4 ,title="Pre-matching Results",type="text", align=TRUE)
 
 # get heteroskedastic std. errors
 library("sandwich")
 m1.rob.se <- sqrt(diag(vcovHC(m1, type = "HC1")))
 m2.rob.se <- sqrt(diag(vcovHC(m2, type = "HC1")))
 m3.rob.se <- sqrt(diag(vcovHC(m3, type = "HC1")))
+m4.rob.se <- sqrt(diag(vcovHC(m4, type = "HC1")))
 
-se.list <- list(NULL, NULL, NULL,
-                m1.rob.se, m2.rob.se, m3.rob.se)
+se.list <- list(NULL, NULL, NULL, NULL,
+                m1.rob.se, m2.rob.se, m3.rob.se, m4.rob.se)
 
 # Compare the standard errors
 # call function with align=TRUE for pretty output
-stargazer(m1, m2, m3, m1, m2, m3,
+stargazer(m1, m2, m3, m4, m1, m2, m3, m4,
           type = "text",
           se = se.list,
-          dep.var.labels = "Conflict",
-          title = "Regression Table",
-          column.labels=c(rep("default",3),
-                          rep("robust",3)),
+          dep.var.labels = "Forest cover loss",
+          title = "Regression Table before matching",
+          column.labels=c(rep("default",4),
+                          rep("robust",4)),
           omit.stat = c("ser","f"),
           no.space = TRUE, align = TRUE)
 
-se.list2 <- list(m1.rob.se, m2.rob.se, m3.rob.se)
+se.list2 <- list(m1.rob.se, m2.rob.se, m3.rob.se, m4.rob.se)
 # call function with align=TRUE for pretty output
 stargazer(m1, m2, m3,
           type = "text",
           se = se.list,
-          dep.var.labels = "Conflict",
-          title = "Regression Table",
-          column.labels=c(rep("default",3),
-                          rep("robust",3)),
+          dep.var.labels = "Forest cover loss",
+          title = "Regression Table before matching",
+          column.labels=c(rep("default",4),
+                          rep("robust",4)),
           omit.stat = c("ser","f"),
           no.space = TRUE, align = TRUE)
